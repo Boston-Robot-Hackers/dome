609be56ffb89b2d0c99a8aa3cd632ae0032a1a65
Pito Salas <pitosalas@gmail.com>
2025-02-16 11:36:56 -0500

    work in progress

--- src/test_cmdvel.py
+++ src/test_cmdvel.py
@@ -11,8 +11,8 @@ class VelocityController(Node):
     def __init__(self):
         super().__init__('velocity_controller')
         self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
-        self.timer = self.create_timer(0.1, self.timer_callback)  # 10Hz control loop
-        self.velocities = [0.5, 0.4, 0.3, 0.2, 0.1]
+        self.timer = self.create_timer(0.1, self.timer_callback)  # 1Hz control loop
+        self.velocities = [0.4, 0.3, 0.2, 0.1]
         self.current_velocity_index = 0
         self.start_time = time.time()
         self.state = 'forward'  # States: forward, wait, backward
@@ -48,7 +48,7 @@ class VelocityController(Node):
         elif self.state == 'rotate':
             self.get_logger().info('Rotate...')
             if elapsed_time < 2.0:
-                msg.angular.z = math.pi # rotate at 18 degrees per second 
+                msg.angular.z = math.pi/2# rotate at 18 degrees per second 
                 self.publisher.publish(msg)
             else:
                 self.state = 'wait2'
@@ -66,13 +66,20 @@ class VelocityController(Node):
                 msg.linear.x = self.velocities[self.current_velocity_index]
                 self.publisher.publish(msg)
             else:
-                # Move to next velocity
                 self.current_velocity_index += 1
                 if self.current_velocity_index >= len(self.velocities):
                     self.completed = True
                 else:
-                    self.state = 'wait3'
+                    self.state = 'rotate2'
                     self.start_time = current_time
+        elif self.state == 'rotate2':
+            self.get_logger().info('rotate2...')
+            if elapsed_time < 2.0:
+                msg.angular.z = math.pi/2.0 # rotate at 18 degrees per second 
+                self.publisher.publish(msg)
+            else:
+                self.state = 'wait3'
+                self.start_time = time.time()
         elif self.state == 'wait3': 
             if elapsed_time < 1.0:
                 # Send zero velocity during wait
