db9749d56cbfa75494eb5f5a612988554cb34dd4
Pito Salas <pitosalas@gmail.com>
2025-03-05 19:58:29 -0500

    work in progress

--- setup.py
+++ setup.py
@@ -9,11 +9,13 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'),
-         glob('launch/*.launch.*')),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']), (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.*')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro') + glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'urdf', 'mech'), glob('urdf/mech/*.xacro') + glob('urdf/mech/*.urdf')),
+        (os.path.join('share', package_name, 'urdf', 'controllers'), glob('urdf/controllers/*.xacro') + glob('urdf/controllers/*.urdf')),
+        (os.path.join('share', package_name, 'urdf', 'robots'), glob('urdf/robots/*.xacro') + glob('urdf/robots/*.urdf')),
+        (os.path.join('share', package_name, 'urdf', 'sensors'), glob('urdf/sensors/*.xacro') + glob('urdf/sensors/*.urdf')),
 
     ],
     install_requires=['setuptools'],
