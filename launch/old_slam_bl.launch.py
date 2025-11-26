dfd376dda826fb57d11c101c705193dbcaf9a886
Pito Salas <pitosalas@gmail.com>
2025-11-26 10:54:25 -0500

    some cleanup

--- launch/old_slam_bl.launch.py
+++ launch/old_slam_bl.launch.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python3
 # This is my version of launching the slam launch file from linorobot2
 
+import os
 
+from ament_index_python.packages import get_package_share_directory
 from better_launch import BetterLaunch, launch_this
 
 
 @launch_this(ui=True)
 def start_nav(rviz_arg: bool = False):
     bl = BetterLaunch()
-    slam_config = bl.find("dome", "slam.yaml")
-    nav_config = bl.find("dome", "navigation.yaml")
+    dome_path = get_package_share_directory("dome")
 
-    bl.include(
-        "linorobot2_navigation",
-        "slam.launch.py",
-        rviz=rviz_arg,
-        sim=False,
-        slam_config=slam_config,
-        nav_config=nav_config,
-    )
-    bl.logger.info("***********")
+    slam_config = os.path.join(dome_path, "config", "slam.yaml")
+
+    nav_config = os.path.join(dome_path, "config", "navigation.yaml")
 
     bl.include(
         "linorobot2_navigation",
--- launch/slam_bl.launch.py
+++ launch/slam_bl.launch.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
 # This is my version of launching the slam launch file from linorobot2
 
-import os
-
-from ament_index_python.packages import get_package_share_directory
 from better_launch import BetterLaunch, launch_this
 
 
 @launch_this(ui=True)
 def start_nav(rviz_arg: bool = False):
     bl = BetterLaunch()
-    dome_path = get_package_share_directory("dome")
+    slam_config = bl.find("dome", "slam.yaml")
+    nav_config = bl.find("dome", "navigation.yaml")
 
-    slam_config = os.path.join(dome_path, "config", "slam.yaml")
-
-    nav_config = os.path.join(dome_path, "config", "navigation.yaml")
+    bl.include(
+        "linorobot2_navigation",
+        "slam.launch.py",
+        rviz=rviz_arg,
+        sim=False,
+        slam_config=slam_config,
+        nav_config=nav_config,
+    )
+    bl.logger.info("***********")
 
     bl.include(
         "linorobot2_navigation",
