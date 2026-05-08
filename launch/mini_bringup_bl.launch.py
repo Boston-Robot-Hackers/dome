0834f4d56f40e35e623fb9354bb2bc35a33ed832
Pito Salas <pitosalas@gmail.com>
2026-05-08 09:41:45 -0400

    small edit

--- launch/mini_bringup_bl.launch.py
+++ launch/mini_bringup_bl.launch.py
@@ -25,6 +25,6 @@ def start_nav():
     bl.include(
         "foxglove_bridge",
         "foxglove_bridge_launch.xml",
-        port="8765"
+        port="8080"
     )
     bl.logger.info(f"****{urdf_path}*******")
